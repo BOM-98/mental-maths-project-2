a0409933c9b9c57ec96dce9090b1c4399a16f3e3
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-05-18 17:48:18 +0000

    feat: add gameLevelSettings array to indicate at each level of the game which parameters to use by referencing the digitParamters object

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,7 @@
 
 let answerArray = [];
 let answerInstance;
+let gameLevel;
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -326,7 +327,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
     },
         division: {
-            1: {
+        21: {
                 num1: {min: 1, max: 12},
                 num2: {min: 1, max: 12}
             },
@@ -347,5 +348,34 @@ document.addEventListener("DOMContentLoaded", function () {
                 num2: {min: 1, max: 1000}
             }
     }
+    }
+
+    let gameLevelSettings = [
+        // Levels 0-1: Basic Level Addition and Subtraction
+        /*0*/ [digitParameters.addition[1], digitParameters.subtract[1]],
+        /*1*/[digitParameters.addition[2], digitParameters.subtract[2]],
+    
+        // Levels 2-11: Intermediate Level Addition, Subtraction, Multiplication, Division
+        /*2*/[digitParameters.addition[2], digitParameters.subtract[2], digitParameters.multiply[1], digitParameters.division[1]],
+        /*3*/[digitParameters.addition[3], digitParameters.subtract[2], digitParameters.multiply[1], digitParameters.division[1]],
+        /*4*/[digitParameters.addition[3], digitParameters.subtract[3], digitParameters.multiply[1], digitParameters.division[1]],
+        /*5*/[digitParameters.addition[3], digitParameters.subtract[3], digitParameters.multiply[2], digitParameters.division[1]],
+        /*6*/[digitParameters.addition[3], digitParameters.subtract[3], digitParameters.multiply[2], digitParameters.division[2]],
+        /*7*/[digitParameters.addition[3], digitParameters.subtract[3], digitParameters.multiply[2], digitParameters.division[2]],
+        /*8*/[digitParameters.addition[3], digitParameters.subtract[3], digitParameters.multiply[3], digitParameters.division[2]],
+        /*9*/[digitParameters.addition[3], digitParameters.subtract[3], digitParameters.multiply[3], digitParameters.division[3]],
+        /*10*/[digitParameters.addition[4], digitParameters.subtract[3], digitParameters.multiply[3], digitParameters.division[3]],
+        /*12*/[digitParameters.addition[4], digitParameters.subtract[4], digitParameters.multiply[3], digitParameters.division[3]],
+        /*13*/[digitParameters.addition[4], digitParameters.subtract[4], digitParameters.multiply[4], digitParameters.division[3]],
+        /*14*/[digitParameters.addition[4], digitParameters.subtract[4], digitParameters.multiply[4], digitParameters.division[4]],
+    
+        // Levels 15-18: Genius Level Addition, Subtraction, Multiplication, Division
+        /*15*/[digitParameters.addition[5], digitParameters.subtract[4], digitParameters.multiply[4], digitParameters.division[4]],
+        /*16*/[digitParameters.addition[5], digitParameters.subtract[5], digitParameters.multiply[4], digitParameters.division[4]],
+        /*17*/[digitParameters.addition[5], digitParameters.subtract[5], digitParameters.multiply[5], digitParameters.division[4]],
+        /*18*/[digitParameters.addition[5], digitParameters.subtract[5], digitParameters.multiply[5], digitParameters.division[5]],
+    ];
+
+    function generateNum1 (gameType, level){
     }
 
